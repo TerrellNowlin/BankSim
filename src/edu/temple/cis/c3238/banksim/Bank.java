acad415086139efba7004fe1f456c46ee6b75313
unknown <yourGithubEmail@email.com>
2018-10-21 22:27:52 -0400

    changed synch

--- src/edu/temple/cis/c3238/banksim/Bank.java
+++ src/edu/temple/cis/c3238/banksim/Bank.java
@@ -33,21 +33,24 @@ public class Bank {
     }
 
     public void test() {
-        int sum = 0;
-        for (Account account : accounts) {
-            System.out.printf("%s %s%n", 
-                    Thread.currentThread().toString(), account.toString());
-            sum += account.getBalance();
-        }
-        System.out.println(Thread.currentThread().toString() + 
-                " Sum: " + sum);
-        if (sum != numAccounts * initialBalance) {
+        synchronized(this)
+        {
+            int sum = 0;
+            for (Account account : accounts) {
+                System.out.printf("%s %s%n", 
+                        Thread.currentThread().toString(), account.toString());
+                sum += account.getBalance();
+            }
             System.out.println(Thread.currentThread().toString() + 
-                    " Money was gained or lost");
-            System.exit(1);
-        } else {
-            System.out.println(Thread.currentThread().toString() + 
-                    " The bank is in balance");
+                    " Sum: " + sum);
+            if (sum != numAccounts * initialBalance) {
+                System.out.println(Thread.currentThread().toString() + 
+                        " Money was gained or lost");
+                System.exit(1);
+            } else {
+                System.out.println(Thread.currentThread().toString() + 
+                        " The bank is in balance");
+            }
         }
     }
 
